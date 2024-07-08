a96527200dc8dd7c9452baf5c4e75c7f829d7837
Andrii <andrii.kondratovich@gmail.com>
2024-07-08 23:02:43 +0300

    init

---
+++ src/index.js
@@ -0,0 +1,48 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+// );
+
+// // If you want to start measuring performance in your app, pass a function
+// // to log results (for example: reportWebVitals(console.log))
+// // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// reportWebVitals();
+
+// const imageUrl =
+//   "https://images.pexels.com/photos/461198/pexels-photo-461198.jpeg?dpr=2&h=480&w=640";
+// const productPrice = 10.99;
+
+// const product = (
+//   <>
+//     <img src={imageUrl} alt="Tacos With Lime" width="640" />
+//     <h2>Tacos With Lime</h2>
+//     <p>Price: {productPrice}$</p>
+//     <button type="button">Add to cart</button>
+//   </>
+// );
+
+// ReactDOM.createRoot(document.getElementById("root")).render(product);
+
+const Product = props => (
+  <>
+    <img
+      src="https://images.pexels.com/photos/461198/pexels-photo-461198.jpeg?dpr=2&h=480&w=640"
+      alt="Tacos With Lime"
+      width="640"
+    />
+    <h2>Tacos With Lime</h2>
+    <p>Price: 10.99$</p>
+    <button type="button">Add to cart</button>
+  </>
+);
+
+// У розмітці компонент записується як JSX-тег
+ReactDOM.createRoot(document.getElementById("root")).render(<Product />);
