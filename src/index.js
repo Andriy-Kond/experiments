bda5933460cf232b10f767be8202c4df63379247
Andrii <andrii.kondratovich@gmail.com>
2024-07-12 09:54:18 +0300

    add: options

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
-import React from "react";
+// import React from "react";
 import ReactDOM from "react-dom/client";
+// import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
@@ -31,18 +32,57 @@ import reportWebVitals from "./reportWebVitals";
 
 // ReactDOM.createRoot(document.getElementById("root")).render(product);
 
-const Product = props => (
-  <>
-    <img
-      src="https://images.pexels.com/photos/461198/pexels-photo-461198.jpeg?dpr=2&h=480&w=640"
-      alt="Tacos With Lime"
-      width="640"
-    />
-    <h2>Tacos With Lime</h2>
-    <p>Price: 10.99$</p>
-    <button type="button">Add to cart</button>
-  </>
-);
+// const element = React.createElement("div", {
+//   a: 6,
+//   b: 10,
+//   children: "Привіт, світ",
+// });
+// console.log("element:::", element);
 
-// У розмітці компонент записується як JSX-тег
-ReactDOM.createRoot(document.getElementById("root")).render(<Product />);
+// ReactDOM.render(element, document.querySelector("#root"));
+
+///////////////////////////
+// const elem1 = <span>Привіт</span>;
+// const elem2 = <span>, світ</span>;
+// const jsxElement = (
+//   <div>
+//     {elem1}
+//     {2 + 2}
+//     {elem2}
+//   </div>
+// );
+
+// // ReactDOM.render(jsxElement, document.querySelector("#root"));
+// ReactDOM.createRoot(document.getElementById("root")).render(jsxElement);
+
+////////////////////////
+
+function Element(props) {
+  console.log("Element >> props:::", props);
+  return (
+    <div>
+      <span>Привіт</span>;<span>, світ</span>;<p>Autor:</p>
+    </div>
+  );
+}
+
+ReactDOM.createRoot(document.querySelector("#root")).render(
+  <Element a="5" b={10} />,
+);
+/////////////////////////
+
+// const Product = props => (
+//   <>
+//     <img
+//       src="https://images.pexels.com/photos/461198/pexels-photo-461198.jpeg?dpr=2&h=480&w=640"
+//       alt="Tacos With Lime"
+//       width="640"
+//     />
+//     <h2>Tacos With Lime</h2>
+//     <p>Price: 10.99$</p>
+//     <button type="button">Add to cart</button>
+//   </>
+// );
+
+// // У розмітці компонент записується як JSX-тег
+// ReactDOM.createRoot(document.getElementById("root")).render(<Product />);
